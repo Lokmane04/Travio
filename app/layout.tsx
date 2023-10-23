d254020f458d830150e316faa1feab9e1ed25247
ReD <baslilokmane04@gmail.com>
2023-10-23 21:46:59 +0100

    fixing an issue with debug.log files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,12 @@
+import React from 'react';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import { ThemeProvider } from '@mui/material';
 
 import NavBar from '../components/navbar/NavBar';
-
 // eslint-disable-next-line import/extensions
-import theme from '@/theme';
+import theme from '../theme';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +19,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <link rel="icon" href="/images/default-logo.svg" type="image/svg" />
+
       <ThemeProvider theme={theme}>
         <body className={inter.className} style={{ margin: '0 100px' }}>
           <NavBar />
