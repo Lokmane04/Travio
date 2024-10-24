6b67834673ade38b7643b1014b22adc70cf9af8c
Lokmane elhakim <baslilokmane04@gmail.com>
2024-10-24 01:16:56 +0100

    deps : bcrypt, jwt & cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,18 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import "dotenv/config";
+import mongoose from "mongoose";
+import userRoutes from "./routes/users";
+import authRoutes from "./routes/auth";
+import path from "path";
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => {
+    console.log("connected to mongodb");
+  })
+  .catch((err) => {
+    console.log("a problem occured", err);
+  });
 
 const app = express();
 
@@ -8,10 +20,11 @@ app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/test", (req: Request, res: Response) => {
-  res.json({ message: "hi there!" });
+app.use("/api/users", userRoutes);
+app.use("/api/auth", authRoutes);
+app.get("*", (req: Request, res: Response) => {
+  res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
 });
-
 app.listen(7000, () => {
   console.log("server running!");
 });
