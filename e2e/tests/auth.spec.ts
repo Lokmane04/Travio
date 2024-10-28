e1ee879a7ebdd0d9d0edb16dbcddc991b46686ac
Lokmane elhakim <baslilokmane04@gmail.com>
2024-10-28 15:21:07 +0100

    test(auth) : Register and Login

--- e2e/tests/auth.spec.ts
+++ e2e/tests/auth.spec.ts
@@ -10,4 +10,23 @@ test("allow user to sign in", async ({ page }) => {
   await page.locator("[name=password]").fill("aaaaaaaa");
   await page.getByRole("button", { name: "Login" }).click();
   await expect(page.getByText("Homepage")).toBeVisible();
+  await expect(page.getByRole("link", { name: "My bookings" })).toBeVisible();
+  await expect(page.getByRole("link", { name: "My hotels" })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Sign Out" })).toBeVisible();
+});
+test("allow user to register", async ({ page }) => {
+  await page.goto(UI_URL);
+  await page.getByRole("link", { name: "Sign in" }).click();
+  await expect(page.getByRole("heading", { name: "Sign In" })).toBeVisible();
+  await page.getByRole("link", { name: "Create an account here" }).click();
+  await expect(
+    page.getByRole("heading", { name: "Create an Account" })
+  ).toBeVisible();
+  await page.locator("[name=firstName]").fill("lokmane");
+  await page.locator("[name=lastName]").fill("baslimane");
+  await page.locator("[name=email]").fill("lokmane@gmail.com");
+  await page.locator("[name=password]").fill("aaaaaaaa");
+  await page.locator("[name=confirmPassword]").fill("aaaaaaaa");
+  await page.getByRole("button", { name: "Create Account" }).click();
+  await expect(page.getByText("Homepage")).toBeVisible();
 });
