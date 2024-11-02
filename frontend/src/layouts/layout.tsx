f9ff06c4a48d8d092d5a3f14921b961d114a958f
Lokmane elhakim <baslilokmane04@gmail.com>
2024-11-02 13:07:10 +0100

    feat(ui) : search bar

--- frontend/src/layouts/layout.tsx
+++ frontend/src/layouts/layout.tsx
@@ -1,6 +1,7 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Hero from "../components/Hero";
+import SearchBar from "../components/search/SearchBar";
 
 interface Props {
   children: React.ReactNode;
@@ -10,8 +11,10 @@ const Layout = ({ children }: Props) => {
   return (
     <div className="flex flex-col min-h-screen">
       <Header />
-
       <Hero />
+      <div className="container mx-auto">
+        <SearchBar />
+      </div>
       <div className="container mx-auto py-10 flex-1 bg-white">{children}</div>
       <Footer />
     </div>
