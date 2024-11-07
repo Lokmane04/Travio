11daf06941f68c4345c4b565ab14797485969c19
Lokmane elhakim <baslilokmane04@gmail.com>
2024-11-07 01:06:36 +0100

    feat : sorting, filtering & search bar

--- backend/src/routes/hotels.ts
+++ backend/src/routes/hotels.ts
@@ -6,15 +6,34 @@ const router = express.Router();
 
 router.get("/search", async (req: Request, res: Response) => {
   try {
+    const query = constructSearchQuery(req.query);
+
+    let sortedOptions = {};
+
+    switch (req.query.sortedOptions) {
+      case "starRating":
+        sortedOptions = { starRating: -1 };
+        break;
+      case "pricePerNightAsc":
+        sortedOptions = { pricePerNight: -1 };
+        break;
+      case "pricePerNightDesc":
+        sortedOptions = { pricePerNight: -1 };
+        break;
+    }
+
     const pageSize = 5;
     const pageNumber = parseInt(
       req.query.page ? req.query.page.toString() : "1"
     );
     const skip = (pageNumber - 1) * pageSize;
 
-    const hotels = await Hotel.find().skip(skip).limit(pageSize);
+    const hotels = await Hotel.find(query)
+      .sort(sortedOptions)
+      .skip(skip)
+      .limit(pageSize);
 
-    const total = await Hotel.countDocuments();
+    const total = await Hotel.countDocuments(query);
 
     const response: HotelSearchResponse = {
       data: hotels,
--- frontend/src/pages/Search.tsx
+++ frontend/src/pages/Search.tsx
@@ -1,13 +1,21 @@
 import { useQuery } from "react-query";
 import { useSearchContext } from "../contexts/SearchContext";
 import * as apiClient from "../api-client";
-import { useState } from "react";
+import React, { useState } from "react";
 import SearchResultsCard from "../components/search/SearchCardResults";
 import Pagination from "../components/search/Pagination";
-import { HotelType } from "../../../backend/src/types/hotel";
+import StarRatingFilter from "../components/filters/StarRating";
+import HotelTypesFilter from "../components/filters/HotelTypesFilter";
+import FacilitiesFilter from "../components/filters/FacilitiesFilter";
+import PriceFilter from "../components/filters/PriceFilter";
 const Search = () => {
   const search = useSearchContext();
   const [page, setPage] = useState<number>(1);
+  const [selectedStars, setSelectedStars] = useState<string[]>([]);
+  const [selectedHotelTypes, setSelectedHotelTypes] = useState<string[]>([]);
+  const [selectedFacilities, setSelectedFacilities] = useState<string[]>([]);
+  const [selectedPrice, setSelectedPrice] = useState<number | undefined>();
+  const [sortOption, setSortOption] = useState<string>("");
   const searchParams = {
     destination: search.destination,
     checkIn: search.checkIn.toISOString(),
@@ -15,17 +23,39 @@ const Search = () => {
     adultCount: search.adultCount.toString(),
     childCount: search.childCount.toString(),
     page: page.toString(),
+    stars: selectedStars,
+    types: selectedHotelTypes,
+    maxPrice: selectedPrice?.toString(),
   };
-  console.log(searchParams.page);
 
   const { data: hotelData } = useQuery(["searchHotels", searchParams], () =>
     apiClient.searchHotels(searchParams)
   );
-  console.log(
-    hotelData?.data.map((star: HotelType) => {
-      return typeof star.starRating;
-    })
-  );
+  const handleStarsChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const starRating = e.target.value;
+    setSelectedStars((prev) =>
+      e.target.checked
+        ? [...prev, starRating]
+        : prev.filter((star) => star !== starRating)
+    );
+  };
+  const handleTypesChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const type = e.target.value;
+    setSelectedHotelTypes((prev) =>
+      e.target.checked
+        ? [...prev, type]
+        : prev.filter((hotel) => hotel !== type)
+    );
+  };
+  const handleFacilityChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const facility = event.target.value;
+
+    setSelectedFacilities((prevFacilities) =>
+      event.target.checked
+        ? [...prevFacilities, facility]
+        : prevFacilities.filter((prevFacility) => prevFacility !== facility)
+    );
+  };
   return (
     <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
       <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
@@ -33,14 +63,50 @@ const Search = () => {
           <h3 className="text-lg font-semibold border-b border-slate-300 pb-5">
             Filter by:
           </h3>
+          <StarRatingFilter
+            selectedStars={selectedStars}
+            onChange={handleStarsChange}
+          />
+          <HotelTypesFilter
+            selectedHotelTypes={selectedHotelTypes}
+            onChange={handleTypesChange}
+          />
+          <FacilitiesFilter
+            selectedFacilities={selectedFacilities}
+            onChange={handleFacilityChange}
+          />
+          <PriceFilter
+            selectedPrice={selectedPrice}
+            onChange={(value?: number) => setSelectedPrice(value)}
+          />
         </div>
       </div>
       <div className="flex flex-col gap-5">
         <div className="flex justify-between items-center">
           <span className="text-xl font-bold">
-            {hotelData?.pagination.total} Hotels found
+            {hotelData?.pagination.total !== 0
+              ? `${hotelData?.pagination.total} `
+              : "No "}
+            Hotels found
             {search.destination ? ` in ${search.destination}` : ""}
           </span>
+          <select
+            value={sortOption}
+            onChange={(event) => setSortOption(event.target.value)}
+            className="p-2 border rounded-md"
+          >
+            <option value="">Sort By</option>
+
+            <option value="starRating">Star Rating</option>
+
+            {/* Sort by Price */}
+            <option value="pricePerNightAsc">
+              &#8593; Price Per Night (Low to High)
+            </option>
+            <option value="pricePerNightDesc">
+              &#8595; Price Per Night (High to Low)
+            </option>
+          </select>
         </div>
         {hotelData?.data.map((hotel, i) => (
           <SearchResultsCard key={i} hotel={hotel} />
